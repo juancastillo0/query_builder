d4134886d1bf2222644605e10cda80fbaa19bb0c
Juan Manuel Castillo <jm.castillo12@uniandes.edu.co>
2021-09-24 18:44:33 +0000

    add conneciton utilities

--- lib/query_builder.dart
+++ lib/query_builder.dart
@@ -3,7 +3,7 @@
 /// More dartdocs go here.
 library query_builder;
 
-export 'src/query_builder_base.dart';
 export 'database/models/sql_values.dart';
+export 'src/query_builder_base.dart';
 
 // TODO: Export any libraries intended for clients of this package.
